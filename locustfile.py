c61e5ae488be20cf679db325e07aa5a9c4dc338d
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-11-05 00:06:23 -0300

    refector: criado rodom para cidades de usuarios

--- locustfile.py
+++ locustfile.py
@@ -2,10 +2,15 @@ from locust import HttpUser, task, between
 import random
 import string
 
-
 def random_string(length=8):
     return ''.join(random.choices(string.ascii_lowercase, k=length))
 
+# Lista de cidades para escolha aleatória
+CIDADES = [
+    "São Paulo", "Rio de Janeiro", "Belo Horizonte", "Curitiba",
+    "Porto Alegre", "Salvador", "Brasília", "Fortaleza",
+    "Recife", "Manaus", "Goiania", "Belém"
+]
 
 class PessoaLoadTest(HttpUser):
     wait_time = between(1, 3)
@@ -13,11 +18,13 @@ class PessoaLoadTest(HttpUser):
     @task
     def create_pessoa(self):
         fake_name = random_string()
+        fake_cidade = random.choice(CIDADES)
+
         json_body = {
             "nome": fake_name.capitalize(),
             "email": f"{fake_name}@teste.com",
             "senha": random_string(10),
-            "cidade": "São Paulo"
+            "cidade": fake_cidade
         }
 
         with self.client.post("/pessoas/", json=json_body, catch_response=True) as res:
